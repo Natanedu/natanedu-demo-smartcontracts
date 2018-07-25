e46a0b982f7a072a8a2fde5049900275329cbb2e
Haythem Sellami <sellamihaythem96@gmail.com>
2018-07-26 00:10:29 +0100

    add ownable

--- contracts/Student.sol
+++ contracts/Student.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.4;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract Student {
+contract Student is Ownable {
 
     mapping(address => bool) whiteListed;
     mapping(address => bool) blackListed;
@@ -12,12 +12,12 @@ contract Student {
     constructor() {
     }
 
-    function whiteList(address _studentAdd) public {
+    function whiteList(address _studentAdd) public onlyOwner {
         require(_studentAdd != address(0), "Invalid address");
         whiteListed[_studentAdd] = true;
     }
 
-    function blackList(address _studentAdd) public {
+    function blackList(address _studentAdd) public onlyOwner {
         require(_studentAdd != address(0), "Invalid address");
         blackListed[_studentAdd] = true;
     }
