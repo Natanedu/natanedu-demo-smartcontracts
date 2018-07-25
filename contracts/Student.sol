5ba45f77698097c7f7406c2c7f59319ffb1a5bbe
Haythem Sellami <sellamihaythem96@gmail.com>
2018-07-26 00:06:43 +0100

    require for valid address

--- contracts/Student.sol
+++ contracts/Student.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.4;
 
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Student {
 
@@ -12,10 +13,12 @@ contract Student {
     }
 
     function whiteList(address _studentAdd) public {
+        require(_studentAdd != address(0), "Invalid address");
         whiteListed[_studentAdd] = true;
     }
 
     function blackList(address _studentAdd) public {
+        require(_studentAdd != address(0), "Invalid address");
         blackListed[_studentAdd] = true;
     }
 
