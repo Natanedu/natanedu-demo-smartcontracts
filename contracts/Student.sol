9f764f3616efa5547d6e9b7ff1db3311dec96111
Haythem Sellami <sellamihaythem96@gmail.com>
2018-07-25 19:06:31 +0100

    whitelist/blacklist student

---
+++ contracts/Student.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.4;
+
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Student {
+
+    mapping(address => bool) whiteListed;
+    mapping(address => bool) blackListed;
+    
+    constructor() {
+        // constructor
+    }
+
+    function whiteList(address _studentAdd) public {
+        whiteListed[_studentAdd] = true;
+    }
+
+    function blackList(address _studentAdd) public {
+        blackListed[_studentAdd] = true;
+    }
+
+}
