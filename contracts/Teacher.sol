e3f0690647b3079d551b6b373986e2ce855b687a
Haythem Sellami <sellamihaythem96@gmail.com>
2018-07-26 00:41:07 +0100

    whitelist/blacklist teacher

--- contracts/Teacher.sol
+++ contracts/Teacher.sol
@@ -1,8 +1,34 @@
 pragma solidity ^0.4.4;
 
 
-contract Teacher {
-  function Teacher() {
-    // constructor
-  }
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract Teacher is Ownable {
+
+    mapping(address => bool) whiteListed;
+    mapping(address => bool) blackListed;
+    
+    constructor() {
+        // constructor
+    }
+
+    /**
+    * @dev function to whitelist a teacher
+    * @param _teacherAdd address of teacher
+    */
+    function whiteList(address _teacherAdd) public onlyOwner {
+        require(_teacherAdd != address(0), "Invalid address");
+        whiteListed[_teacherAdd] = true;
+    }
+
+    /**
+    * @dev function to blacklist a teacher
+    * @param _teacherAdd address of teacher
+    */
+    function blackList(address _teacherAdd) public onlyOwner {
+        require(_teacherAdd != address(0), "Invalid address");
+        blackListed[_teacherAdd] = true;
+    }
+
 }
