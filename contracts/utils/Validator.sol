089e404dd86a7fdabba617d4160d9c533d3e5c81
Haythem Sellami <sellamihaythem96@gmail.com>
2018-08-03 23:38:26 +0100

    modify prefixed func

--- contracts/utils/Validator.sol
+++ contracts/utils/Validator.sol
@@ -7,7 +7,7 @@ contract Validator {
 
     // Builds a prefixed hash to mimic the behavior of eth_sign.
     function prefixed(bytes32 hash) internal pure returns (bytes32) {
-        return keccak256("\x19Ethereum Signed Message:\n32", hash);
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
     }
 
     /**
