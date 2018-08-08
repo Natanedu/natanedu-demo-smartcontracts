4a6211a679a4733327e0c20a16f9158a6f92be6b
Haythem Sellami <sellamihaythem96@gmail.com>
2018-08-09 00:24:44 +0100

    modify payLecture function

--- contracts/NatanLecture.sol
+++ contracts/NatanLecture.sol
@@ -3,18 +3,19 @@ pragma solidity ^0.4.4;
 
 contract NatanLecture {
 
-    mapping(uint => bool) payedLecture;
+    mapping(uint => mapping(address => bool)) payedLecture;
     mapping(uint => bytes) recordedLecture;
     mapping(uint => mapping(address => bool)) accessLecture;
     
 
     function payLecture(uint _lectureId, uint _lecturePrice) external payable {
         require(msg.value == _lecturePrice, "insufficient amount of ether for lecture price");
-        payedLecture[_lectureId] = true;
+        payedLecture[_lectureId][msg.sender] = true;
     }
 
     function saveRecordedLecture(uint _lectureId, bytes _ipfsHash) public {
         recordedLecture[_lectureId] = _ipfsHash;
+        //restrict lecture access
         accessLecture[_lectureId][msg.sender] = true;
     }
 
