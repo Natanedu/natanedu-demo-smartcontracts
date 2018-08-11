b52e645c0be3e54fcf8076dbd789c25acba1ff89
Haythem Sellami <sellamihaythem96@gmail.com>
2018-08-11 21:09:31 +0100

    deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,13 @@
+var NatanAdmin = artifacts.require("./NatanAdmin.sol");
+var NatanStudent = artifacts.require("./NatanStudent.sol");
+var NatanTeacher = artifacts.require("./NatanTeacher.sol");
+var Validator = artifacts.require("./Validator.sol");
+
 async function doDeploy(deployer) {
+    await deployer.deploy(NatanAdmin);
+    await deployer.deploy(NatanStudent);
+    await deployer.deploy(NatanTeacher);
+    await deployer.deploy(Validator);
 }
 
 module.exports = (deployer) => {
