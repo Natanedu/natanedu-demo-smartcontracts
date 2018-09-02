e63caff5c9b54a8a15411a8f3829b96fa4d3aa4a
Haythem Sellami <sellamihaythem96@gmail.com>
2018-09-02 11:15:39 +0100

    register student

--- contracts/NatanStudent.sol
+++ contracts/NatanStudent.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.4;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./utils/Control.sol";
-//import "./NatanLecture.sol";
 
 /**
  * @title NatanStudent
  */
 contract NatanStudent is Control {
 
+    event StudentRegistered(address indexed student);
     event StudentWhitelisted(address indexed student);
     event StudentBlacklisted(address indexed student);
 
@@ -29,12 +29,26 @@ contract NatanStudent is Control {
      */ 
     mapping(address => uint) public listedStudents;
 
+    function registerStudent(address _add, string _name, string _lastName) public {
+        require(_add != address(0), "Invalid address");
+        require(listedStudents[_add] == 0, "Student already registered");
+
+        //add student
+        students[_add] = Student(_name, _lastName);
+        //mark student listing in progress
+        listedStudents[_add] = 2;
+
+        emit StudentRegistered(_add);
+    }
+
     /**
     * @dev function to whitelist a student
     * @param _studentAdd address of student
     */
     function whiteListStudent(address _studentAdd) external onlyOwner {
         require(_studentAdd != address(0), "Invalid address");
+        require(listedStudents[_studentAdd] == 2, "Student not found!");
+
         listedStudents[_studentAdd] = 3;
 
         emit StudentWhitelisted(_studentAdd);
@@ -47,6 +61,7 @@ contract NatanStudent is Control {
     function blackListStudent(address _studentAdd) external onlyOwner {
         require(_studentAdd != address(0), "Invalid address");
         require((listedStudents[_studentAdd] == 3) || (listedStudents[_studentAdd] == 2), "student is not available");
+        
         listedStudents[_studentAdd] = 1;
 
         emit StudentBlacklisted(_studentAdd);
