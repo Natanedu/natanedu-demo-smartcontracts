5938ada93146e11bcf55f7083e2d77ca5e8e6b51
Haythem Sellami <sellamihaythem96@gmail.com>
2018-12-04 14:59:13 +0100

    deploy smart contracts to ropsten network

--- truffle.js
+++ truffle.js
@@ -1,5 +1,6 @@
-require('babel-register');
-require('babel-polyfill');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config()
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +10,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 3000000,
+      //gasPrice: 21
     }
   },
   solc: {
